ad2a5622e572a9fa1040c5548c3d715062a2b9a3
Adam Watchmaker <watchmaker1337@gmail.com>
2023-03-21 16:27:42 +0100

    add css to ExpCard

--- src/components/Workout/Workout.tsx
+++ src/components/Workout/Workout.tsx
@@ -4,7 +4,7 @@ import { ExpCard } from '../common/ExpCard/ExpCard';
 export function Workout() {
   return (
     <div className="container">
-      <ExpCard currLvl={4} category="Workout" userName="Marek" currExp={100} expToLvl={500} executedTask={121} />
+      <ExpCard currLvl={4} category="Workout" userName="Marek szklarzzzzz" currExp={100} expToLvl={500} executedTask={121} />
     </div>
   );
 }
--- src/components/common/ExpCard/ExpCard.tsx
+++ src/components/common/ExpCard/ExpCard.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import './ExpCard.css';
+
 interface Props{
     currLvl:number;
     category:string;
@@ -21,28 +23,30 @@ export function ExpCard(props:Props) {
           <span className="curr_lvl">{currLvl}</span>
         </div>
         <div className="user_container">
-          <h2>{userName}</h2>
+          <h2 className="user_name">{userName}</h2>
         </div>
       </div>
-      <h2>
-        {category}
-        {' '}
-        Experience
-      </h2>
-      <div className="exp_bar_container">
-        <div className="exp_bar">
-          Exp Bar to implement
+      <div className="down_panel">
+        <h2 className="category_name">
+          {category}
+          {' '}
+          Experience
+        </h2>
+        <div className="exp_bar_container">
+          <div className="exp_bar">
+            Exp Bar to implement
+          </div>
         </div>
-      </div>
-      <div className="curr_exp_container">
-        <span className="curr_exp">{currExp}</span>
-        /
-        <span className="curr_exp">{expToLvl}</span>
-        EXP
-      </div>
-      <div className="executed_task_container">
-        Executed Task:
-        <span className="executed_task">{executedTask}</span>
+        <div className="curr_exp_container">
+          <span className="curr_exp">{currExp}</span>
+          /
+          <span className="exp_to_lvl">{expToLvl}</span>
+          EXP
+        </div>
+        <div className="executed_task_container">
+          Executed Task:
+          <span className="executed_task">{executedTask}</span>
+        </div>
       </div>
     </div>
   );
