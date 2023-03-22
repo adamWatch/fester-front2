14b2e385e468dee7e7cc39798948c2c215323230
Adam Watchmaker <watchmaker1337@gmail.com>
2023-03-22 18:12:42 +0100

    add second baner

--- src/components/Workout/Workout.tsx
+++ src/components/Workout/Workout.tsx
@@ -6,66 +6,69 @@ import '../styles/categories.css';
 
 export function Workout() {
   return (
-    <div className="categories_container">
-      <table className="task_table">
-        <thead>
-          <tr>
-            <th>Task</th>
-            <th>Dificulty</th>
-            <th>Exp</th>
-            <th>Done</th>
-            <th>Delete</th>
+    <>
+      <h2 className="second_baner">Workout is good</h2>
+      <div className="categories_container">
+        <table className="task_table">
+          <thead>
+            <tr>
+              <th>Task</th>
+              <th>Dificulty</th>
+              <th>Exp</th>
+              <th>Done</th>
+              <th>Delete</th>
 
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <SingleTask text="push-up and two hang upppppppppppppppppppppppppp" />
-            <td>Hard</td>
-            <td>15</td>
-            <td>BTN</td>
-            <td>BTN_Delete</td>
-          </tr>
-          <tr>
-            <SingleTask text="push-up" />
-            <td>Hard</td>
-            <td>15</td>
-            <td>BTN</td>
-            <td>BTN_Delete</td>
-          </tr>
-          <tr>
-            <SingleTask text="push-up" />
-            <td>Hard</td>
-            <td>15</td>
-            <td>BTN</td>
-            <td>BTN_Delete</td>
-          </tr>
-          <tr>
-            <SingleTask text="push-up" />
-            <td>Hard</td>
-            <td>15</td>
-            <td>BTN</td>
-            <td>BTN_Delete</td>
-          </tr>
-          <tr>
-            <SingleTask text="push-up" />
-            <td>Hard</td>
-            <td>15</td>
-            <td>BTN</td>
-            <td>BTN_Delete</td>
-          </tr>
-          <tr>
-            <SingleTask text="push-up" />
-            <td>Hard</td>
-            <td>15</td>
-            <td>BTN</td>
-            <td>BTN_Delete</td>
-          </tr>
-        </tbody>
-      </table>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <SingleTask text="push-up and two hang upppppppppppppppppppppppppp" />
+              <td>Hard</td>
+              <td>15</td>
+              <td>BTN</td>
+              <td>BTN_Delete</td>
+            </tr>
+            <tr>
+              <SingleTask text="push-up" />
+              <td>Hard</td>
+              <td>15</td>
+              <td>BTN</td>
+              <td>BTN_Delete</td>
+            </tr>
+            <tr>
+              <SingleTask text="push-up" />
+              <td>Hard</td>
+              <td>15</td>
+              <td>BTN</td>
+              <td>BTN_Delete</td>
+            </tr>
+            <tr>
+              <SingleTask text="push-up" />
+              <td>Hard</td>
+              <td>15</td>
+              <td>BTN</td>
+              <td>BTN_Delete</td>
+            </tr>
+            <tr>
+              <SingleTask text="push-up" />
+              <td>Hard</td>
+              <td>15</td>
+              <td>BTN</td>
+              <td>BTN_Delete</td>
+            </tr>
+            <tr>
+              <SingleTask text="push-up" />
+              <td>Hard</td>
+              <td>15</td>
+              <td>BTN</td>
+              <td>BTN_Delete</td>
+            </tr>
+          </tbody>
+        </table>
 
-      <ExpCard currLvl={4} category="Workout" userName="Marek szklarzzzzz" currExp={100} expToLvl={500} executedTask={121} />
+        <ExpCard currLvl={4} category="Workout" userName="Marek szklarzzzzz" currExp={100} expToLvl={500} executedTask={121} />
 
-    </div>
+      </div>
+    </>
   );
 }
