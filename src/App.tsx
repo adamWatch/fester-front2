1754317a9418d3b571d53ebb7fff1949327380eb
Adam Watchmaker <watchmaker1337@gmail.com>
2023-04-01 15:16:27 +0200

    add login Router

--- src/App.tsx
+++ src/App.tsx
@@ -12,17 +12,30 @@ import { Recreation } from './components/Recreation/Recreation';
 import { Hobby } from './components/Hobby/Hobby';
 import { LogginContext } from './components/Context/LogginContext';
 import { Login } from './components/views/Login/Login';
+import { Register } from './components/views/Register/Register';
 
 export function App() {
-  const [isLogged, setIsLogged] = useState(false);
+  const [isLogged, setIsLogged] = useState(true);
+
+  if (isLogged === false) {
+    return (
+      <LogginContext.Provider value={{ isLogged, setIsLogged }}>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </LogginContext.Provider>
+
+    );
+  }
 
   return (
     <div className="App">
       <Headers />
       <LogginContext.Provider value={{ isLogged, setIsLogged }}>
+
         <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/Home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/workout" element={<Workout />} />
           <Route path="/learning" element={<Learning />} />
           <Route path="/nutrition" element={<Nutrition />} />
