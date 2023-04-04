dac0e2dc8963de307b578c905da418bc31c8de51
Adam Watchmaker <watchmaker1337@gmail.com>
2023-04-04 17:15:51 +0200

    add register form and fetch

--- src/components/views/Register/Register.tsx
+++ src/components/views/Register/Register.tsx
@@ -1,9 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, SyntheticEvent, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Btn } from '../../common/Btn/Btn';
-import './Register.css';
+import '../../styles/LoginRegister.css';
+import { Notice } from '../../common/Notice/Notice';
+import { validationForm } from '../../utils/validationForm';
 
 export function Register() {
+  const [notice, setNotice] = useState({
+    isVisible: false,
+    noticeText: '',
+    noticeColor: '',
+  });
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setNotice(() => ({
+        isVisible: false,
+        noticeText: '',
+        noticeColor: '',
+      }));
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, [notice]);
+
   const [form, setForm] = useState({
     username: '',
     email: '',
@@ -17,11 +37,39 @@ export function Register() {
     }));
   };
 
+  const sendForm = async (e:SyntheticEvent) => {
+    e.preventDefault();
+    const notice = validationForm('register', form);
+    setNotice(() => ({
+      isVisible: notice.isVisible,
+      noticeText: notice.noticeText,
+      noticeColor: notice.noticeColor,
+    }));
+    if (notice.noticeColor === 'red') return;
+
+    const res = await fetch('http://localhost:3021/register', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(form),
+
+    });
+
+    const data = await res.json();
+
+    setNotice(() => ({
+      isVisible: data.isVisible,
+      noticeText: data.noticeText,
+      noticeColor: data.noticeColor,
+    }));
+  };
+
   return (
     <div className="login_view">
       <h1 className="baner">Motivator</h1>
-      <h2>Log In</h2>
-      <form className="login_form">
+      <h2 className="type_baner">Register</h2>
+      <form className="login_form" onSubmit={sendForm}>
         <label htmlFor="username">
           Username:
           <input type="text" id="username" name="username" required value={form.username} onChange={(e) => updateForm('username', e.target.value)} />
@@ -35,12 +83,14 @@ export function Register() {
           Password:
           <input type="password" id="password" name="password" required value={form.password} onChange={(e) => updateForm('password', e.target.value)} />
         </label>
-        <Btn text="Login" />
+        <Btn text="Register" typeBtn="submit" />
       </form>
       <div className="" />
       You have account
       {' '}
-      <Link to="/">Log In</Link>
+      <Link className="link" to="/">Log In</Link>
+      {notice.isVisible && <Notice text={notice.noticeText} color={notice.noticeColor} />}
     </div>
+
   );
 }
