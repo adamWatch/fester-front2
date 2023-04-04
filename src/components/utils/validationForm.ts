0c75ae1c89504eb13a0abd49c22385f50915fa83
Adam Watchmaker <watchmaker1337@gmail.com>
2023-04-04 17:43:16 +0200

    fix typescript bug

--- src/components/utils/validationForm.ts
+++ src/components/utils/validationForm.ts
@@ -7,25 +7,26 @@ export function validationForm(type:string, obj:UserEntity):Notice {
     noticeColor: 'green',
   };
   if (type === 'register') {
-    if (obj.email.length === 0 || obj.email.length > 100) {
-      noticeObj.noticeText = 'Your email is too small or big than 100 char';
-
+    if (obj.username?.length === 0 || obj.email.length > 50) {
+      noticeObj.noticeText = 'Your username is blank or big than 50 char';
+      noticeObj.noticeColor = 'red';
       return noticeObj;
     }
   }
-  if (obj.username.length === 0 || obj.email.length > 50) {
-    noticeObj.noticeText = 'Your username is blank or big than 50 char';
-
+  if (obj.email.length === 0 || obj.email.length > 100) {
+    noticeObj.noticeText = 'Your email is too small or big than 100 char';
+    noticeObj.noticeColor = 'red';
     return noticeObj;
   }
+
   if (obj.password.length === 0 || obj.password.length > 50) {
     noticeObj.noticeText = 'Your password is blank or big than 50 char';
-
+    noticeObj.noticeColor = 'red';
     return noticeObj;
   }
   if (obj.password.length < 8) {
     noticeObj.noticeText = 'Your password must have at least 8 char';
-
+    noticeObj.noticeColor = 'red';
     return noticeObj;
   }
 
