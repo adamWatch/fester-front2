d65283fe157193cb9fa696b57b69b8ade9bbdeb4
Adam Watchmaker <watchmaker1337@gmail.com>
2023-04-08 14:38:33 +0200

    add handle exp card

--- src/components/common/ExpCard/ExpCard.tsx
+++ src/components/common/ExpCard/ExpCard.tsx
@@ -1,27 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ExpHandle } from 'types';
 import { ProgressBar } from '../ProgressBar/ProgressBar';
 
 import './ExpCard.css';
 
-interface Props{
-    currLvl:number;
-    category:string;
-    userName:string;
-    currExp:number;
-    expToLvl:number;
-    executedTask:number;
+interface Props {
+  category: string;
+  userName: string;
+  currExp: number;
+  executedTask: number;
 }
 
-export function ExpCard(props:Props) {
+export function ExpCard(props: Props) {
   const {
-    currLvl, category, userName, currExp, expToLvl, executedTask,
+    category, userName, currExp, executedTask,
   } = props;
+
+  const [categoryExp, setCategoryExp] = useState<ExpHandle>({
+    currLvl: 1,
+    currExp,
+    expToLvl: 400,
+
+  });
+  const [progressBar, setProgressBar] = useState('0');
+
+  const handleExp = (currExp:number, currLvl:number) => {
+    const nextLvlIncrement = 50;
+    let expToLvl = 400;
+    let currLvlIn = currLvl;
+    let currExpIn = currExp;
+
+    if (currExpIn > expToLvl) {
+      while (currExpIn > expToLvl) {
+        currExpIn -= expToLvl;
+        expToLvl += nextLvlIncrement;
+        currLvlIn++;
+      }
+      setCategoryExp(() => ({
+        currLvl: currLvlIn,
+        currExp: currExpIn,
+        expToLvl,
+      }));
+      const progressPerc = String((currExpIn / expToLvl) * 100);
+      const progressBarValue = `${progressPerc}%`;
+      setProgressBar(progressBarValue);
+    }
+  };
+
+  handleExp(categoryExp.currExp, categoryExp.currLvl);
 
   return (
     <div className="exp_card_container">
       <div className="upp_panel">
         <div className="curr_lvl_container">
-          <span className="curr_lvl">{currLvl}</span>
+          <span className="curr_lvl">{categoryExp.currLvl}</span>
         </div>
         <div className="user_container">
           <h2 className="user_name">{userName}</h2>
@@ -33,12 +65,12 @@ export function ExpCard(props:Props) {
           Experience
         </h2>
         <div className="exp_bar_container">
-          <ProgressBar procentOfProgressBar="19%" />
+          <ProgressBar procentOfProgressBar={progressBar} />
         </div>
         <div className="curr_exp_container">
-          <span className="curr_exp">{currExp}</span>
+          <span className="curr_exp">{categoryExp.currExp}</span>
           /
-          <span className="exp_to_lvl">{expToLvl}</span>
+          <span className="exp_to_lvl">{categoryExp.expToLvl}</span>
           EXP
         </div>
         <div className="executed_task_container">
