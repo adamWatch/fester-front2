086dc7c20b4743515fb29f153d9452c0e065ddbe
Adam Watchmaker <watchmaker1337@gmail.com>
2023-04-10 19:45:06 +0200

    fix LogginContext

--- src/components/Context/LogginContext.tsx
+++ src/components/Context/LogginContext.tsx
@@ -1,6 +1,11 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
 import { createContext } from 'react';
 
-export const LogginContext:any = createContext({
-  isLog: false,
-  idLog: '',
-});
+export interface LogginContextType{
+  isLog:boolean;
+  idLog:string;
+}
+
+export const LogginContext = createContext<[LogginContextType, React.Dispatch<React.SetStateAction<LogginContextType>>]>(
+  [{ isLog: false, idLog: '' }, () => {}],
+);
